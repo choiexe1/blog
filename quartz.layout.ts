f040917ffd78e18acd8f592bb563a8be154c3a05
choiexe1 <choiexe1@gmail.com>
2025-01-03 12:42:32 +0900

    vault backup: 2025-01-03 12:42:32

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
-import * as Component from "./quartz/components"
+// quartz.layout.ts
+import { PageLayout, SharedLayout } from "./quartz/cfg";
+import * as Component from "./quartz/components";
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -11,7 +12,7 @@ export const sharedPageComponents: SharedLayout = {
       GitHub: "https://github.com/choiexe1",
     },
   }),
-}
+};
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -26,26 +27,44 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      sortFn: (a, b) => {
+        const dateA = a.file?.frontmatter?.date ? new Date(a.file.frontmatter.date) : new Date(0);
+        const dateB = b.file?.frontmatter?.date ? new Date(b.file.frontmatter.date) : new Date(0);
+
+        return dateB.getTime() - dateA.getTime();
+      },
+      order: ["sort", "filter", "map"],
+    })),
   ],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-
-}
+};
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      sortFn: (a, b) => {
+        const dateA = a.file?.frontmatter?.date ? new Date(a.file.frontmatter.date) : new Date(0);
+        const dateB = b.file?.frontmatter?.date ? new Date(b.file.frontmatter.date) : new Date(0);
+
+        return dateB.getTime() - dateA.getTime();
+      },
+      order: ["sort", "filter", "map"], // 적용할 함수의 순서
+    })),
   ],
   right: [],
-  footer: Component.RecentNotes(),
-}
+};
